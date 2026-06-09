ccf0ded8bbc1f633691c41eeec1a24158b50808b
khella <samaan.khella.ext@nokia.com>
2026-06-09 09:21:02 +0300

    initialized project

---
+++ src/main/java/com/example/deployment_config_manager/DeploymentConfigManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.deployment_config_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentConfigManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentConfigManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deployment_config_manager/DeploymentConfigManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deployment_config_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentConfigManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
