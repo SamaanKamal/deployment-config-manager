4e48c0e3e92f1dcb5ed02d48f84cc10226146da4
khella <samaan.khella.ext@nokia.com>
2026-07-06 12:03:23 +0300

    modified environment entity

--- src/main/java/com/example/deployment_config_manager/Entity/Environment.java
+++ src/main/java/com/example/deployment_config_manager/Entity/Environment.java
@@ -1,24 +1,34 @@
 package com.example.deployment_config_manager.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 @Entity
 @Data
+@Table(name = "environments", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"project_id", "name"})
+})
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Environment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    @ManyToOne
-    @JoinColumn(name = "project_id")
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id" , nullable = false)
     private Project project;
     private String name;
     private String description;
     @CreationTimestamp
-    private Timestamp createdAt;
+    private LocalDateTime createdAt;
     @UpdateTimestamp
-    private Timestamp updatedAt;
+    private LocalDateTime updatedAt;
 }
