80c37d86acd07f523150719bdbbe5701e03599fc
khella <samaan.khella.ext@nokia.com>
2026-07-06 16:15:07 +0300

    removed project id from Update Environment Request dto

--- src/main/java/com/example/deployment_config_manager/DTO/Environment/UpdateEnvironmentRequest.java
+++ src/main/java/com/example/deployment_config_manager/DTO/Environment/UpdateEnvironmentRequest.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateEnvironmentRequest {
-    private Long projectId;
     private String name;
     private String description;
 }
