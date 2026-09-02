7389a0048af2056318142bca635a2b8cfb3437e2
samaan kamal <samaankamal72@gmail.com>
2026-09-02 14:26:19 +0300

    added validation on creation request object for project and environment

--- src/main/java/com/example/deployment_config_manager/DTO/Environment/CreateEnvironmentRequest.java
+++ src/main/java/com/example/deployment_config_manager/DTO/Environment/CreateEnvironmentRequest.java
@@ -1,13 +1,19 @@
 package com.example.deployment_config_manager.DTO.Environment;
 
 import com.example.deployment_config_manager.Entity.Project;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateEnvironmentRequest {
+    @NotNull(message = "Project ID is required")
     private Long projectId;
+    @NotBlank(message = "Environment name is required")
+    @Size(max = 100)
     private String name;
     private String description;
 }
--- src/main/java/com/example/deployment_config_manager/DTO/Project/CreateProjectRequest.java
+++ src/main/java/com/example/deployment_config_manager/DTO/Project/CreateProjectRequest.java
@@ -1,5 +1,7 @@
 package com.example.deployment_config_manager.DTO.Project;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateProjectRequest {
+    @NotBlank(message = "Project name is required")
+    @Size(max = 100, message = "Project name cannot exceed 100 characters")
     private String name;
+    @Size(max = 2000, message = "Description cannot exceed 2000 characters")
     private String description;
 }
